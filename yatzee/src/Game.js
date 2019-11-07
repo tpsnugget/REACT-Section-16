cfc9e5c00a2b1811f396a1025952ecf3d8e644a6
Michael Giebner <mikegiebner@gmail.com>
2019-11-07 18:41:11 -0500

    Finished Part Five

--- yatzee/src/Game.js
+++ yatzee/src/Game.js
@@ -36,13 +36,15 @@ class Game extends Component {
 
   roll(evt) {
     // roll dice whose indexes are in reroll
-    this.setState(st => ({
-      dice: st.dice.map((d, i) =>
-        st.locked[i] ? d : Math.ceil(Math.random() * 6)
-      ),
-      locked: st.rollsLeft > 1 ? st.locked : Array(NUM_DICE).fill(true),
-      rollsLeft: st.rollsLeft - 1
-    }));
+    if(this.state.rollsLeft  !== 0){
+      this.setState(st => ({
+        dice: st.dice.map((d, i) =>
+          st.locked[i] ? d : Math.ceil(Math.random() * 6)
+        ),
+        locked: st.rollsLeft > 1 ? st.locked : Array(NUM_DICE).fill(true),
+        rollsLeft: st.rollsLeft - 1
+      }));
+    }
   }
 
   toggleLocked(idx) {
